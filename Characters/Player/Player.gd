c912d2cc9b9d2af9cf31881f64728c1fbf9c426d
DominikSledzik <dominiksledzik1@gmail.com>
2022-02-17 01:21:01 +0100

    Imported animation and added the wall that will connect to the button that moves them.

--- Characters/Player/Player.gd
+++ Characters/Player/Player.gd
@@ -5,51 +5,48 @@ export var jump_strength := 20.0
 export var gravity := 50.0
 var jumpNum = 0
 
-#var is_alive = true
-var health = 3
-
 var _velocity := Vector3.ZERO
 var _snap_vector := Vector3.DOWN
 
 onready var _spring_arm: SpringArm = $SpringArm
-onready var _model: Spatial = $PlayerSkin
+onready var _model: Spatial = $Mesh
 
 func _physics_process(delta: float) -> void:
-	if health > 0:
-		var move_direction := Vector3.ZERO
-		move_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-		move_direction.z = Input.get_action_strength("back") - Input.get_action_strength("forward")
-		move_direction = move_direction.rotated(Vector3.UP, _spring_arm.rotation.y).normalized()
+	var move_direction := Vector3.ZERO
+	move_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	move_direction.z = Input.get_action_strength("back") - Input.get_action_strength("forward")
+	move_direction = move_direction.rotated(Vector3.UP, _spring_arm.rotation.y).normalized()
+	
+	_velocity.x = move_direction.x * speed
+	_velocity.z = move_direction.z * speed
+	_velocity.y -= gravity * delta
+	
+	var just_landed := is_on_floor() and _snap_vector == Vector3.ZERO
+	var is_jumping := is_on_floor() and Input.is_action_just_pressed("jump")
+	_velocity = move_and_slide_with_snap(_velocity, _snap_vector, Vector3.UP, true)
+	
+
+	if is_on_floor():
+		jumpNum = 0
 		
-		_velocity.x = move_direction.x * speed
-		_velocity.z = move_direction.z * speed
-		_velocity.y -= gravity * delta
+	if is_jumping:
+		if jumpNum == 0:
+			_velocity.y = jump_strength
+			_snap_vector = Vector3.UP
+			jumpNum = 1
 		
-		var just_landed := is_on_floor() and _snap_vector == Vector3.ZERO
-		var is_jumping := is_on_floor() and Input.is_action_just_pressed("jump")
-		_velocity = move_and_slide_with_snap(_velocity, _snap_vector, Vector3.UP, true)
+	if Input.is_action_just_pressed("jump") and not is_on_floor():
+		if jumpNum == 1:
+			_velocity.y = jump_strength
+			_snap_vector = Vector3.UP
+			jumpNum = 2
 		
-		if is_on_floor():
-			jumpNum = 0
+	elif just_landed:
+		_snap_vector = Vector3.DOWN
 		
-		if is_jumping:
-			if jumpNum == 0:
-				_velocity.y = jump_strength
-				_snap_vector = Vector3.UP
-				jumpNum = 1
-		
-		if Input.is_action_just_pressed("jump") and not is_on_floor():
-			if jumpNum == 1:
-				_velocity.y = jump_strength
-				_snap_vector = Vector3.UP
-				jumpNum = 2
-				
-		elif just_landed:
-			_snap_vector = Vector3.DOWN
-		
-		if _velocity.length() > 0.2:
-			var look_direction = Vector2(_velocity.z, _velocity.x)
-			_model.rotation.y = look_direction.angle()
+	if _velocity.length() > 0.2:
+		var look_direction = Vector2(_velocity.z, _velocity.x)
+		_model.rotation.y = look_direction.angle()
 
 
 func _process(_delta: float) -> void:
